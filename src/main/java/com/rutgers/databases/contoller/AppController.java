2b9a12389b68660bb166a55f347169815d5dec0b
fatima AlSaadeh <fatiemahsaadeh993@gmail.com>
2019-09-30 12:09:09 -0400

    Initial Changes

---
+++ src/main/java/com/rutgers/databases/contoller/AppController.java
@@ -0,0 +1,19 @@
+package com.rutgers.databases.contoller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home.html";
+    }
+
+}
---
+++ src/main/java/com/rutgers/databases/entity/Order_Products.java
@@ -0,0 +1,55 @@
+//package com.rutgers.databases.entity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="order_products")
+//public class Order_Products {
+//    @Id
+//    @Column(name = "product_id")
+//    private Integer productId;
+//
+//    @Column(name = "order_id")
+//    private Integer order_id;
+//
+//    @Column(name = "add_to_cart_order")
+//    private Integer add_to_cart_order;
+//
+//    @Column(name = "reorder")
+//    private String reorder;
+//
+//    public Integer getProductId() {
+//        return productId;
+//    }
+//
+//    public void setProductId(Integer productId) {
+//        this.productId = productId;
+//    }
+//
+//    public Integer getOrder_id() {
+//        return order_id;
+//    }
+//
+//    public void setOrder_id(Integer order_id) {
+//        this.order_id = order_id;
+//    }
+//
+//    public Integer getAdd_to_cart_order() {
+//        return add_to_cart_order;
+//    }
+//
+//    public void setAdd_to_cart_order(Integer add_to_cart_order) {
+//        this.add_to_cart_order = add_to_cart_order;
+//    }
+//
+//    public String getReorder() {
+//        return reorder;
+//    }
+//
+//    public void setReorder(String reorder) {
+//        this.reorder = reorder;
+//    }
+//}
